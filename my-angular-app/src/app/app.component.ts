eacdc720afa95c88853f1c2e6680a8d12b79add8
shubham prasad <anmol.raj3167@gmail.com>
2021-06-13 22:36:38 +0530

    Angular Apps

--- my-angular-app/src/app/app.component.ts
+++ my-angular-app/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/Common/http';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-angular-app';
+  User : Object;
+
+  constructor(private http : HttpClient){  }
+
+  ngOnInit(){ 
+    this.getUser();
+   }
+
+  getUser() : void{
+
+    this.http.get<Observable<Object>>('https://datausa.io/api/data?drilldowns=Nation&measures=Population').subscribe(response =>{
+      this.User = response;}
+      ,error=>{
+      console.log(error);
+    })
+  }
+
+
+
 }
+
+
